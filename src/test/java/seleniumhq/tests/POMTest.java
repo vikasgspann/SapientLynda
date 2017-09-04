d4efd816dcb90c50c40a059ef3b9f2d7b50b24e1
Vikas Singh 4 <vsi134@sapient.com>
2017-09-04 14:20:54 +0530

    assert pass

--- src/test/java/seleniumhq/tests/POMTest.java
+++ src/test/java/seleniumhq/tests/POMTest.java
@@ -28,7 +28,7 @@ public class POMTest {
         DocumentationPage dp = hp.documentationLinkClick();
         TestDesignConsiderationPage tdcp = dp.testDesignConsiderationClick();
         String expected = tdcp.pageObjectDesignPatternHeadlineString();
-        String actual = "Page Object Design Pattern - Wrong one";
+        String actual = "Page Object Design Pattern";
 
         Assert.assertEquals(expected,actual);
     }
